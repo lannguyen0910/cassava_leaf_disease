58d8b28ecaaa6716261a3a32a1760ed37ff73fea
lan.nguyen2k <47696901+lannguyen0910@users.noreply.github.com>
2021-04-01 10:04:22 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cassvana Leaf Classification with Pytorch Custom Template for Image Classification
+
+## To-do list:
+- [ ] Semi-supervised integrations
+- [ ] Gradient checkpointing
+- [ ] Distributed data parallel
+- [ ] Sync BatchNorm
+- [x] Multi-GPU support (nn.DataParallel)
+- [x] GradCAM vizualization
+- [x] Gradient Accumulation
+- [x] Mixed precision
+
+## Reference:
+- timm models from https://github.com/rwightman/pytorch-image-models
---
+++ utils/random_seed.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+import random
+import os
+
+SEED = 1702
+# Inherited from https://github.com/vltanh/pytorch-template
+
+
+def seed_everything(seed=SEED, device=True):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    if device:
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
+        torch.backends.cudnn.fastest = True
+    else:
+        torch.manual_seed(seed)
